1f54d7602118a1bfc645337d059a5de5f968060c
MokkeMeguru <meguru.mokke@gmail.com>
2019-12-31 16:16:55 +0900

    add doc

---
+++ README.md
@@ -0,0 +1,8 @@
+# portfolio
+
+勿怪巡のポートフォリオサイトです。
+
+
+
+# Created by
+[re-frame](https://github.com/day8/re-frame)
---
+++ src/cljs/portfolio/core.cljs
@@ -0,0 +1,23 @@
+(ns portfolio.core
+  (:require
+   [reagent.core :as reagent]
+   [re-frame.core :as re-frame]
+   [portfolio.events :as events]
+   [portfolio.views :as views]
+   [portfolio.config :as config]
+   ))
+
+
+(defn dev-setup []
+  (when config/debug?
+    (println "dev mode")))
+
+(defn ^:dev/after-load mount-root []
+  (re-frame/clear-subscription-cache!)
+  (reagent/render [views/main-panel]
+                  (.getElementById js/document "app")))
+
+(defn init []
+  (re-frame/dispatch-sync [::events/initialize-db])
+  (dev-setup)
+  (mount-root))
